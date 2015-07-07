a49a2e8bf99f96758f59767b1e3a0972ac312b9a
rafuchos <rafaelcunha75@gmail.com>
2015-07-07 11:11:16 -0300

    fetch

--- codigo/estagio_decode.cpp
+++ codigo/estagio_decode.cpp
@@ -1,5 +1,10 @@
 #include "systemc.h"
 #include "estagio_decode.h"
 
-	void estagio_decode::
+	void estagio_decode::decodeinst(){
+
+		RsD= (InstD << 7)>>32;
+		RtD= (InstD << 12)>>32;
+		RdD= (InstD << 17)>>32;
+		
 
